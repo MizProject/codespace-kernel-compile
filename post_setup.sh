af5d8080d03f20a2134726e695ac0ffbbca06298
SUFandom <70519093+SUFandom@users.noreply.github.com>
2024-08-27 14:55:13 +0000

    added storage

--- post_setup.sh
+++ post_setup.sh
@@ -52,7 +52,62 @@ function setup_codebase_sys_swap() {
     fi
 }
 
-
-
+function setup_storage_space() {
+    # You want expanded storage?? Ill give you expanded storage...
+    # Idk if microsoft would patch this even i guess
+    if [ -e /tmp/disk_loop ] && [ -e /workspaces/diskmnt ]; then
+        echo "No need to setup storage space... its already available"
+        echo "df -h /workspaces/diskmnt"
+        df -h /workspaces/diskmnt
+        echo "df -h /tmp/disk_loop"
+        df -h /tmp/disk_loop
+    else
+        echo "Size Available: None[0], 24GB [24] and 40GB [40]"
+        read -p "Choose what additional storage size you wanted, you can ignore this if you want... (Warning: The space/disk will be reset after codespace ends itselfm, suggest you should now wha you are doing)" STOR
+        case $STOR in
+            "24")
+                echo "Wait..."
+                mkdir -p /workspaces/diskmnt
+                dd if=/dev/zero of=/tmp/disk_loop bs=1024 count=25769803776 status=progress
+                echo "Done creating, now formatting and mounting"
+                echo "Setting up disks"
+                LS="$(losetup -f)"
+                losetup "$LS" /tmp/disk_loop
+                mkfs.ext4 "$LS"
+                echo "Mounting on /workspaces/diskmnt"
+                mount -o defaults,rw "$LS" /workspaces/diskmnt
+                echo "Printing disk info to output_disks.txt"
+                tune2fs -l "$LS" > output_disks.txt
+                echo "Done"
+                echo "Disks created + mounted"
+                ;;
+            "40")
+                echo "Wait..."
+                mkdir -p /workspaces/diskmnt
+                dd if=/dev/zero of=/tmp/disk_loop bs=1024 count=42949672960 status=progress
+                echo "Done creating, now formatting and mounting"
+                echo "Setting up disks"
+                LS="$(losetup -f)"
+                losetup "$LS" /tmp/disk_loop
+                mkfs.ext4 "$LS"
+                echo "Mounting on /workspaces/diskmnt"
+                mount -o defaults,rw "$LS" /workspaces/diskmnt
+                echo "Printing disk info to output_disks.txt"
+                tune2fs -l "$LS" > output_disks.txt
+                echo "Done"
+                echo "Disks created + mounted"
+                ;;  
+            "0")
+                echo "Aborted creating disks"
+                ;;
+            *)
+                echo "$STOR is invalid"
+                echo "Only allowed: 0 / 24 / 40"
+                exit 1
+                ;;
+        esac
+    fi
+}
 
 setup_codebase_sys_swap
+setup_storage_space
