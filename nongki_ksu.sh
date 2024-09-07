1a49c0bcf2061439fa34142120faeee287b27918
SUFandom <70519093+SUFandom@users.noreply.github.com>
2024-09-07 20:57:12 +0000

    improved script

--- nongki_ksu.sh
+++ nongki_ksu.sh
@@ -28,13 +28,16 @@ case "$1" in
         echo "v1"
         echo ""
         echo "Usage:"
-        echo "./nongki_ksu.sh --<args>"
+        echo "./nongki_ksu.sh --<args1> --<args2>"
         echo ""
         echo "Arguments"
         echo "  --help -h                   Shows this help page"
         echo "  --kernel:<version>          Uses the patch method readily available"
         echo "  --download-move-and-link    Self explainatory, just grab repo"
         echo "                              move it, and link it (not including patching)"
+        echo "  --list-conf                 Lists all available configuration"
+        echo "                              That is device-specific. released by contributors"
+        echo "                              "
         echo ""
         echo "Patch:"
         echo "  Usage:"
@@ -46,6 +49,15 @@ case "$1" in
         echo "      4.14 Kernel     --kernel:4.14"
         echo "      4.19 Kernel     --kernel:4.19"
         echo ""
+        echo "Additional Args:"
+        echo ""
+        echo "Configuration Variants:"
+        echo "  Usages:"
+        echo "      ./nongki_ksu.sh --kernel:4.19 --conf:a12s"
+        echo ""
+        echo "It's the Secondary argument, meant for device-specific configurations only"
+        echo "Provided by Contributors"
+        echo ""
         echo "For GKI Kernels, do check with tiann's installer script instead"
         echo "Earlier than 4.4 isn't supported"
         echo ""
@@ -54,6 +66,15 @@ case "$1" in
     "--download-move-and-link")
         SPECIAL_TRIGGER=1
         ;;
+    "--list-conf")
+            echo ""
+            echo "Available configurations: "
+            echo ""
+            echo " --conf:a12s              Configuration Made by @SUFandom"
+            echo ""
+            echo "Your device conf isn't here? Do make a push at https://github.com/SUFandom/KernelSU"
+            echo ""
+            ;;
     *)
         ./nongki_ksu.sh --help
         exit
@@ -92,10 +113,12 @@ function linkit() {
 
 function patch() {
     cd .. # Force redir to repo
-    
-    git apply --stat --check "KernelSU/hook_patch/$HOOK_TARGET"
-    echo "DFS"
-    git apply --stat --check "KernelSU/hook_patch/Kcm-4.19-a12s.diff"
+    if [ -z "$2" ]; then
+        git apply --stat --check "KernelSU/hook_patch/$HOOK_TARGET"
+    elif [ "$2" == "--conf:a12s"  ]
+        git apply --stat --check "KernelSU/hook_patch/4.19_hook_patch-a12s.diff"
+    fi
+    git apply --stat --check "KernelSU/hook_patch/Kcm-4.19-a12s.diff" # This requires jsut adding 2 ksu variables
 }
 function revert_dir_location() {
     cd $DIRROOT
